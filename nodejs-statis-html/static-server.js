b4977bbeada6c7535f56c453b7df2c32af0abbab
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2020-08-06 13:39:48 +0700

    ok

--- nodejs-statis-html/static-server.js
+++ nodejs-statis-html/static-server.js
@@ -6,18 +6,38 @@ http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
 
-    // baca file
-    fs.readFile(filename, function(err, data) {
-        if (err) { // kirim balasan error
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        } 
-        // kirim balasan dengan isi file statis
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-  
+    console.log( q );
+
+    switch ( req.url){
+        case '/':
+            fs.readFile("index.html", (err, data) => {
+                if (err) { // kirim balasan error
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    return res.end("404 Not Found");
+                } 
+                // kirim form login_form.html
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+            break;
+        default:
+            // baca file
+            fs.readFile(filename, function(err, data) {
+                if (err) { // kirim balasan error
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    return res.end("404 Not Found");
+                } 
+                // kirim balasan dengan isi file statis
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+        
+    }
+    
+    
+    
 }).listen(8000);
 
 console.log('server is running on http://localhost:8000');
