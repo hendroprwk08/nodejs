af305ef0f0dd6d0af295bb002aa3de71410a1d62
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2020-09-02 21:43:37 +0700

    OK

--- restful-api/index.js
+++ restful-api/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const mysql = require('mysql');
-
-// parse application/json
-app.use(bodyParser.json());
 
 //create database connection
 const conn = mysql.createConnection({
@@ -25,7 +21,8 @@ app.get('/api/barang',(req, res) => {
   let sql = "SELECT * FROM barang";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({"status": 200, "response": results}));
   });
 });
 
@@ -34,38 +31,39 @@ app.get('/api/barang/:id',(req, res) => {
   let sql = "SELECT * FROM barang WHERE idbarang='"+ req.params.id +"'";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({"status": 200, "response": results}));
   });
 });
 
 //Tambahkan data product baru
 app.post('/api/barang',(req, res) => {
-  let data = {idbarang: req.body.idbarang, 
-                namabarang: req.body.namabarang,
-                hargabeli: req.body.hargabeli,
-                hargajual: req.body.hargajual,
-                stok: req.body.stok,
-                idsupplier: req.body.idsupplier,
-                expired: req.body.expired
-              };  
+  var data = req.query;
+  //console.log(data);
 
-  let sql = "INSERT INTO barang SET ?";
+  let sql = "INSERT INTO barang values('"+ data.id +"', '"+ data.nama +"', " 
+            + data.beli +", "+ data.jual +", "+ data.stok +", '"+ data.supplier +"', "
+            + "'"+ data.expired +"')";
 
-  let query = conn.query(sql, data,(err, results) => {
+  let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({"status": 200, "response": results}));
   });
 });
 
 //Edit data product berdasarkan id
-app.put('/api/barang/:id',(req, res) => {
-  let sql = "UPDATE barang SET namabarang = '"+ req.body.namabarang +"', hargabeli = "+ req.body.product_price +",  ";
-  sql += "hargajual = "+ req.body.product_price +", stok = "+ req.body.stok +", idsupplier = '"+ req.body.idsupplier +"', ";
-  sql += "expired = '"+ req.body.expired +"' WHERE idbarang= '"+ req.body.idbarang + "'";
+app.put('/api/barang/',(req, res) => {
+  var data = req.query;
+  
+  let sql = "UPDATE barang SET namabarang = '"+ data.nama +"', hargabeli = "+ data.beli +",  ";
+  sql += "hargajual = "+ data.jual +", stok = "+ data.stok +", idsupplier = '"+ data.supplier +"', ";
+  sql += "expired = '"+ data.expired +"' WHERE idbarang = '"+ data.id + "'";
 
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({"status": 200, "response": results}));
   });
 });
 
@@ -74,7 +72,8 @@ app.delete('/api/barang/:id',(req, res) => {
   let sql = "DELETE FROM barang WHERE idbarang ='"+req.params.id+"'";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({"status": 200, "response": results}));
   });
 });
 
